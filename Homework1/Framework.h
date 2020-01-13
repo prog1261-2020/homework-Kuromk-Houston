93d21fc495dfbe7a07e7dd0d1161a17e1f41f9ce
Kuromk-Houston <54894272+Kuromk-Houston@users.noreply.github.com>
2020-01-13 14:50:40 -0400

    Homework1 Initial Upload

---
+++ Homework1/Framework.h
@@ -0,0 +1,32 @@
+/*
+* @file
+* @author  <Brandon Houston Brandonhouston2593@gmail.com>
+* @version 1.0
+*
+*
+* @section DESCRIPTION
+* < brief description of the purpose of this file >
+*
+*
+* @section LICENSE
+* <any necessary attributions>
+*
+* Copyright 2019 <Brandon Houston>
+* Permission to use, copy, modify, and/or distribute this software for
+* any purpose with or without fee is hereby granted, provided that the
+* above copyright notice and this permission notice appear in all copies.
+*
+* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+* WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+* ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*
+* @section Academic Integrity
+*  I certify that this work is solely my own and complies with
+*  NBCC Academic Integrity Policy (policy 1111)
+*/
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
